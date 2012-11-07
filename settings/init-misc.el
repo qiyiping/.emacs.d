4555d652fa7d663c59f98d103854e3e636ecbfb1
qiyiping <qiyiping@gmail.com>
2012-11-07 14:36:56 +0800

    add chinese calendar

--- settings/init-misc.el
+++ settings/init-misc.el
@@ -20,4 +20,10 @@
 
 (random t)
 
+;; chinese calendar
+(require 'cal-china-x)
+(setq mark-holidays-in-calendar t)
+(setq cal-china-x-priority1-holidays cal-china-x-chinese-holidays)
+(setq calendar-holidays cal-china-x-priority1-holidays)
+
 (provide 'init-misc)
--- settings/init-yasnippet.el
+++ settings/init-yasnippet.el
@@ -2,9 +2,9 @@
 (require 'yasnippet)
 (yas-global-mode t)
 
-;; remove the annoying yas/x-prompt
-(setq yas/prompt-functions '(yas/dropdown-prompt
-			     yas/ido-prompt
-			     yas/completing-prompt))
+;; Remove the annoying yas-x-prompt
+(setq yas-prompt-functions '(yas-dropdown-prompt
+			     yas-ido-prompt
+			     yas-completing-prompt))
 
 (provide 'init-yasnippet)
