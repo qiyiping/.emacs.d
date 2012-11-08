5f314c7c83c12a5c4ab04f1ab80f9a6022d737f3
qiyiping <qiyiping@gmail.com>
2012-11-08 13:14:05 +0800

    use http://github.com/fgallina/python.el

--- settings/init-python.el
+++ settings/init-python.el
@@ -1,5 +1,9 @@
 ;;; init-python.el --- python settings
 
+;; Use python.el from http://github.com/fgallina/python.el
+;; Use the master branch in emacs24 and the emacs23 branch in emacs23
+(require 'python)
+
 (add-to-list 'auto-mode-alist '("SConstruct\\'" . python-mode))
 (add-to-list 'auto-mode-alist '("YBUILD\\'" . python-mode))
 
