aefcc497c56bf096d581ac14854ade7865de088a
qiyiping <qiyiping@gmail.com>
2012-11-08 13:52:19 +0800

    add a key binding for browse url

--- settings/init-misc.el
+++ settings/init-misc.el
@@ -20,21 +20,24 @@
 
 (random t)
 
-;; chinese calendar
+;; Chinese calendar
 (require 'cal-china-x)
 (setq mark-holidays-in-calendar t)
 (setq cal-china-x-priority1-holidays cal-china-x-chinese-holidays)
 (setq calendar-holidays cal-china-x-priority1-holidays)
 
-;; tramp
+;; Tramp
 (require 'tramp)
 (setq tramp-default-method "ssh")
 
+;; Allow Emacs to be a server for client processes
 ;; (server-start)
 (defun kill-emacs-server ()
   "Save buffers and kill emacs server"
   (interactive)
   (save-some-buffers)
   (kill-emacs))
+
+(global-set-key [f12] 'browse-url-at-point)
 
 (provide 'init-misc)
