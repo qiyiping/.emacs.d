bb75730456a17e2de45e0c930c7e6187a35256ae
qiyiping <qiyiping@gmail.com>
2012-11-18 20:45:26 +0800

    modify default font size

--- settings/init-ui.el
+++ settings/init-ui.el
@@ -49,6 +49,6 @@
   (transparency 93)
   ;; Set font size
   ;; Use 'C-x C-+'/'C-x C--' to adjust the font size
-  (set-face-attribute 'default nil :height 130))
+  (set-face-attribute 'default nil :height 140))
 
 (provide 'init-ui)
