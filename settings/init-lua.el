aeda775ef1df8db8fa26b684fbbea28e34a601f4
qiyiping <qiyiping@gmail.com>
2013-01-03 22:24:45 +0800

    add lua mode

--- settings/init-lua.el
+++ settings/init-lua.el
@@ -1,3 +1,7 @@
 ;;; init-lua.el --- lua settings
 
-(require 'lua-mode)
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+(provide 'init-lua)
