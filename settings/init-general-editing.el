98fc6662fe32a3d35717d600d782c320ac877a1e
qiyiping <qiyiping@gmail.com>
2013-04-24 11:39:36 +0800

    add functions to enable open file with external applications in dired

--- settings/init-general-editing.el
+++ settings/init-general-editing.el
@@ -55,6 +55,23 @@
 (put 'dired-find-alternate-file 'disabled nil)
 (setq dired-dwim-target t)
 
+(defun open-file-externally (file-name)
+  (cond
+   ((eq system-type 'windows-nt)
+    (w32-shell-execute "open" file-name))
+   ((eq system-type 'darwin)
+    (shell-command (format "open %s" file-name)))
+   ((eq system-type 'gnu/linux)
+    (let ((process-connection-type nil))
+      (start-process "" nil "xdg-open" file-name)))))
+
+(defun my-dired-find-file (&optional prefix)
+  (interactive "P")
+  (if prefix
+      (open-file-externally (dired-get-file-for-visit))
+    (dired-find-file)))
+(define-key dired-mode-map "\r" 'my-dired-find-file)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Some handy edit toolkit
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
