dee5bd642e64239db2f8bbf82771bc0eae9df6af
qiyiping <qiyiping@gmail.com>
2013-05-09 20:18:17 +0800

    add org mode in package archives

--- settings/init-package.el
+++ settings/init-package.el
@@ -2,6 +2,7 @@
 
 (require 'package)
 
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (package-initialize)
