784ed289acb4ab783e63ce8dc84ed9dc15c36f5f
qiyiping <qiyiping@gmail.com>
2014-08-21 23:06:49 +0800

    add scala mode

--- init.el
+++ init.el
@@ -27,6 +27,7 @@
 (require 'init-markdown)
 (require 'init-eim)
 (require 'init-xml)
+(require 'init-scala)
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ settings/init-scala.el
@@ -0,0 +1,6 @@
+;;; init-scala.el scala settings
+
+(require 'scala-mode2)
+(require 'sbt-mode)
+
+(provide 'init-scala)
