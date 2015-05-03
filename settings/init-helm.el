e168a36279eb09ea41e856c12eacbec9c314ea8a
qiyiping <qiyiping@gmail.com>
2015-05-03 22:02:49 +0800

    update helm config

--- settings/init-helm.el
+++ settings/init-helm.el
@@ -24,6 +24,17 @@
       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
       helm-ff-file-name-history-use-recentf t)
 
+(global-set-key (kbd "C-x b") 'helm-mini)
+(setq helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match    t)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(setq helm-M-x-fuzzy-match t) ;; optional fuzzy matching for helm-M-x
+
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
 (helm-mode 1)
 
 (provide 'init-helm)
