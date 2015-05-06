5ee07bf6dffe6902ac9c53363442c242138e4abf
qiyiping <qiyiping@meituan.com>
2015-05-06 14:10:18 +0800

    update auto complete conf

--- settings/init-auto-complete.el
+++ settings/init-auto-complete.el
@@ -1,10 +1,10 @@
 (require 'auto-complete-config)
 (ac-config-default)
 
-(require 'auto-complete-clang)
+;; (require 'auto-complete-clang)
 
-(defun my-ac-cc-mode-setup ()
-  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
-(add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
+;; (defun my-ac-cc-mode-setup ()
+;;   (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
+;; (add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
 
 (provide 'init-auto-complete)
