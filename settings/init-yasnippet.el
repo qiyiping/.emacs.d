2701acdc3219ffbcf8612b44d00c9b96643bf102
qiyiping <qiyiping@meituan.com>
2015-06-12 11:52:08 +0800

    enable yas-minor mode

--- settings/init-yasnippet.el
+++ settings/init-yasnippet.el
@@ -1,10 +1,10 @@
 ;;; init-yasnippet.el -- yasnippet settings
 (require 'yasnippet)
 (yas-global-mode t)
-
+(yas-minor-mode-on)
 ;; Remove the annoying yas-x-prompt
-(setq yas-prompt-functions '(yas-dropdown-prompt
-			     yas-ido-prompt
-			     yas-completing-prompt))
+;; (setq yas-prompt-functions '(yas-dropdown-prompt
+;; 			     yas-ido-prompt
+;; 			     yas-completing-prompt))
 
 (provide 'init-yasnippet)
