b9d8974583314b807bc479044e68db779da3bac8
qiyiping <qiyiping@qiyipingdeMacBook-Pro.local>
2015-11-06 15:06:24 +0800

    use the default configure in python.el for ipython

--- settings/init-python.el
+++ settings/init-python.el
@@ -9,17 +9,8 @@
 (add-to-list 'auto-mode-alist '("SConstruct\\'" . python-mode))
 (add-to-list 'auto-mode-alist '("YBUILD\\'" . python-mode))
 
-(setq
- python-shell-interpreter "ipython"
- python-shell-interpreter-args ""
- python-shell-prompt-regexp "In \\[[0-9]+\\]: "
- python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
- python-shell-completion-setup-code
- "from IPython.core.completerlib import module_completion"
- python-shell-completion-module-string-code
- "';'.join(module_completion('''%s'''))\n"
- python-shell-completion-string-code
- "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i")
 
 (setq python-eldoc-setup-code
   "def __PYDOC_get_help(obj):
