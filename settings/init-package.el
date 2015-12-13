f02d15f5ddafc9072abd973278c4b62ed6ba48f5
qiyiping <qiyiping@gmail.com>
2015-12-13 12:20:46 +0800

    add helm-package

--- settings/init-package.el
+++ settings/init-package.el
@@ -14,6 +14,7 @@
 			  google-c-style
                           helm
                           helm-projectile
+                          helm-bibtex
                           nyan-mode
 			  auctex
                           ace-jump-mode
