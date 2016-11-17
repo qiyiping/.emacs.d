3a22d417dc873bf52a36a08b636f7db85e881fd9
qiyiping <qiyiping@meituan.com>
2016-11-17 13:04:27 +0800

    set `eww' buffer name

--- settings/init-misc.el
+++ settings/init-misc.el
@@ -35,6 +35,17 @@
 ;; browser support
 (global-set-key [f8] 'browse-url-at-point)
 
+(defun my-set-eww-buffer-title ()
+  (let* ((title  (plist-get eww-data :title))
+         (url    (plist-get eww-data :url))
+         (result (concat "*eww-" (or title
+                              (if (string-match "://" url)
+                                  (substring url (match-beginning 0))
+                                url)) "*")))
+    (rename-buffer result t)))
+
+(add-hook 'eww-after-render-hook 'my-set-eww-buffer-title)
+
 ;; sudo edit
 (defun sudo-edit ()
   (interactive)
